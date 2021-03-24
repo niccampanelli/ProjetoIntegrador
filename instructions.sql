517e2b150f833d4581d7da8ac8e3b81bd7ea65bd
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-03-23 23:10:18 -0300

    Adicionando trigger

--- instructions.sql
+++ instructions.sql
@@ -1,4 +1,4 @@
-CREATE TABLE livros
+CREATE TABLE if not exists livros
 (
     id int primary key,
     nome varchar(255),
@@ -6,3 +6,16 @@ CREATE TABLE livros
     publicacao date,
     descri text
 );
+
+CREATE OR REPLACE FUNCTION conferirDados()  RETURNS trigger AS $conferirDados$
+    BEGIN
+        RAISE EXCEPTION 'A tabela foi alterada';
+    END;
+$conferirDados$ LANGUAGE plpgsql;
+
+CREATE TRIGGER assistirTabelas BEFORE INSERT OR UPDATE ON livros
+FOR EACH ROW EXECUTE PROCEDURE conferirDados();
+
+insert into livros (id, nome, autor, publicacao, descri) values (1, 'livro1', 'autor1', '10/10/2010', 'lorem');
+
+select * from livros;
